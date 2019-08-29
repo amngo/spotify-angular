e3bb072900445abf3254e743516b88bc76c96bb0
Andy Ngo <amngo96@gmail.com>
2019-08-28 18:24:36 -0700

    initital commit

---
+++ client/src/app/pages/track-page/track-page.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { ArtistData } from "../../data/artist-data";
+import { TrackData } from "../../data/track-data";
+import { AlbumData } from "../../data/album-data";
+import { TrackFeatures } from "../../data/track-features";
+import { SpotifyService } from "src/app/services/spotify.service";
+
+@Component({
+    selector: "app-track-page",
+    templateUrl: "./track-page.component.html",
+    styleUrls: ["./track-page.component.css"]
+})
+export class TrackPageComponent implements OnInit {
+    trackId: string;
+    track: TrackData;
+    audioFeatures: TrackFeatures;
+    featureTypes = TrackFeatures.FeatureTypes;
+
+    constructor(
+        private route: ActivatedRoute,
+        private spotifyServce: SpotifyService
+    ) {}
+
+    ngOnInit() {
+        this.trackId = this.route.snapshot.paramMap.get("id");
+        //TODO: Inject the spotifyService and use it to get the track data and it's audio features
+        this.spotifyServce
+            .getTrack(this.trackId)
+            .then(data => (this.track = data));
+
+        this.spotifyServce
+            .getAudioFeaturesForTrack(this.trackId)
+            .then(data => (this.audioFeatures = data));
+    }
+}
